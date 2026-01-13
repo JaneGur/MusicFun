da5972a8c21d42d2ed044789942548120f8f4104
Evgeniya <iliving2007@yandex.ru>
2026-01-13 15:16:26 +0200

    🔶 12. Optimistic update

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -42,11 +42,42 @@ export const playlistsApi = baseApi.injectEndpoints({
             invalidatesTags: ['Playlist']
         }),
         updatePlaylist: build.mutation<void, { playlistId: string; body: UpdatePlaylistArgs }>({
-            query: playlistId => ({
-                url: `playlists/${playlistId}`,
-                method: 'put',
-            }),
-            invalidatesTags: ['Playlist']
+            query: ({ playlistId, body }) => ({ url: `playlists/${playlistId}`, method: 'put', body }),
+            async onQueryStarted({ playlistId, body }, { dispatch, queryFulfilled, getState }) {
+                const args = playlistsApi.util.selectCachedArgsForQuery(getState(), 'fetchPlaylists')
+
+                const patchResults: any[] = []
+
+                args.forEach(arg => {
+                    patchResults.push(
+                        dispatch(
+                            playlistsApi.util.updateQueryData(
+                                'fetchPlaylists',
+                                {
+                                    pageNumber: arg.pageNumber,
+                                    pageSize: arg.pageSize,
+                                    search: arg.search,
+                                },
+                                state => {
+                                    const index = state.data.findIndex(playlist => playlist.id === playlistId)
+                                    if (index !== -1) {
+                                        state.data[index].attributes = { ...state.data[index].attributes, ...body }
+                                    }
+                                }
+                            )
+                        )
+                    )
+                })
+
+                try {
+                    await queryFulfilled
+                } catch {
+                    patchResults.forEach(patchResult => {
+                        patchResult.undo()
+                    })
+                }
+            },
+            invalidatesTags: ['Playlist'],
         }),
         uploadPlaylistCover: build.mutation<Images, { playlistId: string; file: File }>({
             query: ({ playlistId, file }) => {
